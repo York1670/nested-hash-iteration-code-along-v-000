eb6ac9bf0c88648c6665358fd8a5d9a82df69500
Bobbi York <york1670@gmail.com>
2018-10-15 20:51:51 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,21 +14,22 @@ require 'pry'
   #   }
   # }
 
-def remove_strawberry(contacts)
+def remove_strawberry(contacts
   contacts.each do |person, data|
-    data.each do |attribute, value|
-      if attribute == :favorite_ice_cream_flavors
-        value.each do |flavor|
-          i = 0
-          if flavor == "strawberry"
-            i = "strawberry"[i]
-            flavor.delete_at(i)
-          else
-            i += 1
+    if person == "Freddy Mercury"
+      data.each do |attribute, value|
+        if attribute == :favorite_ice_cream_flavors
+          value.each do |flavor|
+            i = 0
+            if flavor == "strawberry"
+              flavor.delete_at(i)
+            else
+              i += 1
+            end
           end
         end
       end
     end
   end
-  return contacts
+    return contacts
 end
